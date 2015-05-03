a391c2cc35033bbada46735a77382ef96726ef5e
mohsenzakeri <mohsen_zakeree@yahoo.com>
2015-05-03 04:00:04 -0700

    QuiteNoChange

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -52,8 +52,8 @@ class GamesController < ApplicationController
       end
     end
     UserGame.create_user_game(current_user,@game)
-    	#@alphabets = ['s','y','z','a','b']
-    	@alphabets = ['الف' , 'ب' , '‍‍‍پ' , 'ت' , 'ث' , 'ج' , 'چ' , 'ح' , 'خ' , 'د' , 'ذ' , 'ر' , 'ز' , 'ژ' , 'س' , 'ش' , 'ص' ,  'ض' ,  'ط' , 'ظ' ,  'ع' , 'غ'  , 'ف'  , 'ق' , 'ک' , 'گ' , 'ال' , 'م' , 'ن' , 'و' , 'ه' , 'ی']
+    	@alphabets = ['s','y','z','a','b']
+    	#@alphabets = ['الف' , 'ب' , '‍‍‍پ' , 'ت' , 'ث' , 'ج' , 'چ' , 'ح' , 'خ' , 'د' , 'ذ' , 'ر' , 'ز' , 'ژ' , 'س' , 'ش' , 'ص' ,  'ض' ,  'ط' , 'ظ' ,  'ع' , 'غ'  , 'ف'  , 'ق' , 'ک' , 'گ' , 'ال' , 'م' , 'ن' , 'و' , 'ه' , 'ی']
 #	
     for i in 1..@game.rounds_num
 	@selected_letter = @alphabets.sample
