0aa9353970b2a4fb36aa556b249b9af87f2c844e
Mahsa <ms.shahshahani@gmail.com>
2015-05-16 06:52:36 -0400

    mail and devise corrected-deploy-heroku

--- db/migrate/20150304052759_create_game_rounds.rb
+++ db/migrate/20150304052759_create_game_rounds.rb
@@ -2,6 +2,17 @@ class CreateGameRounds < ActiveRecord::Migration
   def change
     create_table :game_rounds do |t|
 	  t.integer :score
+	  t.integer :round_number
+	  t.integer :user_game_id
+	  t.string :firstname
+	  t.string :lastname
+	  t.string :city
+          t.string :country
+          t.string :fruit
+          t.string :thing
+          t.string :animal
+	  t.string :flower
+
       t.timestamps
     end
   end
