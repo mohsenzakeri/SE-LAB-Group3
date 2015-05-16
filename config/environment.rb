1ccc64a49186bfd4f597e9ccfe6eac3ebee1a748
Mahsa <ms.shahshahani@gmail.com>
2015-05-16 04:54:27 -0400

    devise-deploy-heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Esmfamil::Application.initialize!
-onfig.action_mailer.default_url_options = { :host => 'https://calm-tundra-9478.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'https://calm-tundra-9478.herokuapp.com' }
