9e714d638a0fb75fcfbf7779d7ea6effd624dedc
Mahsa <ms.shahshahani@gmail.com>
2015-05-16 05:03:09 -0400

    mail-deploy-heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Esmfamil::Application.initialize!
-config.action_mailer.default_url_options = { :host => 'https://calm-tundra-9478.herokuapp.com' }
