c0f7f76377c3df9b558eddc02453bb8591ae8dc4
Mahsa <ms.shahshahani@gmail.com>
2015-05-17 02:25:26 -0400

    database conflicts corrected-deploy

--- db/migrate/20150516203839_add_score_to_game_rounds.rb
+++
@@ -1,5 +0,0 @@
-class AddScoreToGameRounds < ActiveRecord::Migration
-  def change
-    add_column :game_rounds, :score, :integer
-  end
-end
--- db/migrate/20150516204100_add_currently_scored_to_games.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrentlyScoredToGames < ActiveRecord::Migration
-  def change
-    add_column :games, :currently_scored, :integer
-  end
-end
