41cb816d5c3be4384fef6ed1e10fb31bd466ee8e
Une30 <younesi429@yahoo.com>
2015-05-29 15:04:25 +0430

    News' part added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Esmfamil::Application.routes.draw do
+  resources :events
+
   resources :timers
 
   resources :game_judges
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514122052) do
+ActiveRecord::Schema.define(version: 20150529102749) do
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "game_alphabets", force: true do |t|
     t.integer  "game_id"
