03bfb9ee7eee18d4d883ce72d9275b6f1760b5a0
mohsenzakeri <mohsen_zakeree@yahoo.com>
2015-05-30 17:17:04 -0700

    SomeUIRevised2

--- app/models/game_alphabet.rb
+++ app/models/game_alphabet.rb
@@ -4,4 +4,9 @@ class GameAlphabet < ActiveRecord::Base
 	def self.new_game_alphabet(_game_id,_round_number,_alphabet)
 		@game_alphabet = GameAlphabet.create(:game_id=>_game_id, :round_number=>_round_number, :alphabet=>_alphabet)	
 	end
+	
+	def self.delete_game_alphabets(_gma_id)
+		@the_game_alphabets = GameAlphabet.where( :game_id => _game_id)
+		@the_game_alphabets.destroy
+	end
 end
